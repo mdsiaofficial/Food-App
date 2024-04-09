85fcc1dac74f694f5cf94ed5c595d74930c5c43f
mdsiaofficial <mdsiaofficial@gmail.com>
2024-04-09 23:50:20 +0600

    scroll button header border added

--- script.js
+++ script.js
@@ -62,3 +62,55 @@ tabs.forEach(tab=> {
 
     });
 });
+
+
+// scroll up
+
+const scrollUp = () => {
+    const scrollUpBtn = document.getElementById("scroll-up");
+
+    if(this.scrollY>=250){
+        scrollUpBtn.classList.remove("-bottom-1/2");
+        scrollUpBtn.classList.add("bottom-4");
+        
+    }else{
+        scrollUpBtn.classList.add("-bottom-1/2");
+        scrollUpBtn.classList.remove("bottom-4");
+    }
+}
+window.addEventListener("scroll", scrollUp);
+
+
+
+
+// header border bottom
+const scrollHeader = () => {
+    const header = document.getElementById("header");
+
+    if(window.scrollY>=50){
+        
+        // header.classList.remove("borber-b-2", "border-primary");
+        header.classList.add("borber-b-2", "border-primary");
+        
+    }else{
+        header.classList.remove("borber-b-2", "border-primary");
+    }
+};
+window.addEventListener("scroll", scrollHeader);
+
+
+
+// dark light theme
+const html = document.querySelector("html");
+const themeToggle = document.getElementById("theme-toggle");
+
+if(localStorage.getItem("mode") == "dark"){
+
+    themeToggle.addEventListener("click", (e) => {
+        html.classList.add("dark");
+        themeToggle.classList.replace("ri-moon-line", "ri-sun-line");
+        localStorage.setItem("mode", "dark");
+    })
+}
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/index.html/", "*"],
+  darkMode: "class",
   theme: {
     
     screens: {
